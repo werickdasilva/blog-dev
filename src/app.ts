ac6690237760058d708ab4f21b32c840fd9057a6
Werick da Silva Santana <werick.s.santana34@gmail.com>
2023-10-20 04:16:50 -0300

    fixing json bug

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,11 @@ import bodyParser from 'body-parser'
 const PORT = process.env.PORT || 3001
 const app = express()
 
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 app.use(userRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
-
-
 
 app.listen(PORT, () => console.log(`Server running in Port ${PORT}`))
